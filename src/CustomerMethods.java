2c0aace929680c8f9193cec115fdff3b8d85edc6
gianthips <gianthips@gmail.com>
2023-01-17 18:21:24 +0900

    230117

--- src/CustomerMethods.java
+++ src/CustomerMethods.java
@@ -18,8 +18,8 @@ public class CustomerMethods {
 	
 	public Orders orderCheckandMake() {		
 		
-		int q_menunum = 0; //주문 번호가 제대로 입력되었는지 여부를 따지는 index. 0이면 잘못 입력.
-		int q_ordernum = 0; //주문 수량이 제대로 입력되었는지 여부를 따지는 index. 0이면 잘못 입력.
+		int q_menuNum = 0; //주문 번호가 제대로 입력되었는지 여부를 따지는 index. 0이면 잘못 입력.
+		int q_orderNum = 0; //주문 수량이 제대로 입력되었는지 여부를 따지는 index. 0이면 잘못 입력.
 		int orderFinishNum = 0;//주문이 완료되었는지 여부를 따지는 index. 0이면 추가 입력이 있을 예정.
 		int menuNum = 0;
 		int orderNum = 0;
@@ -27,27 +27,27 @@ public class CustomerMethods {
 		Scanner sc = new Scanner(System.in);
 		Checkers ch = new Checkers();
 		
-		while(q_menunum == 0) {
+		while(q_menuNum == 0) {
 			System.out.println("주문번호: ");
 			String menuNumTemp = sc.next();
 			if(ch.menuNumCheck(menuNumTemp)) {
 				menuNum = Integer.parseInt(menuNumTemp);
-				q_menunum = 1;
+				q_menuNum = 1;
 			}else {
 			System.out.println("잘못 입력하셨습니다. 다시 입력하세요.");
-			q_menunum = 0;
+			q_menuNum = 0;
 			}
 		}
 		
-		while(q_ordernum == 0) {
+		while(q_orderNum == 0) {
 			System.out.println("몇 개를 구매하시겠습니까?: ");
 			String orderNumTemp = sc.next();
 			if(ch.orderNumCheck(orderNumTemp)) {
 				orderNum = Integer.parseInt(orderNumTemp);
-				q_ordernum = 1;
+				q_orderNum = 1;
 			}else {
 			System.out.println("잘못 입력하셨습니다. 다시 입력하세요.");
-			q_ordernum = 0;
+			q_orderNum = 0;
 			}			
 		}
 		
@@ -64,11 +64,14 @@ public class CustomerMethods {
 			System.out.println("잘못 입력하셨습니다. 다시 입력하세요.");
 			orderFinishNum = 0;			
 		}
-		
-		
+				
 		return new Orders(menuNum, orderNum, orderFinishNum);
 	}
 	
+	public Orders[] orderTabling() {
+		Orders[] orderTable;
+		
+	}
 	
 }
 	
